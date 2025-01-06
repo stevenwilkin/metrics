85c3c124865a733d0b35848e58117f4b989e0999
Steven Wilkin <stevenwilkin@gmail.com>
2025-01-06 14:04:29 +1100

    Display per existing `premia` command

--- cmd/metrics/main.go
+++ cmd/metrics/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"net/url"
 	"os"
+	"time"
 
 	"github.com/gorilla/websocket"
 	_ "github.com/joho/godotenv/autoload"
@@ -16,6 +17,33 @@ type stateMessage []struct {
 	Price     float64 `json:"price"`
 	Index     float64 `json:"index"`
 	Funding   float64 `json:"funding"`
+}
+
+func tenor(ms int64) string {
+	days := ms / (1000 * 60 * 60 * 24)
+	hours := (ms / (1000 * 60 * 60)) % 24
+	minutes := (ms / (1000 * 60)) % 60
+
+	return fmt.Sprintf("%3dd %2dh %2dm", days, hours, minutes)
+}
+
+func display(sm stateMessage) {
+	fmt.Println("\033[2J\033[H\033[?25l") // clear screen, move cursor to top of screen, hide cursor
+	fmt.Println("")
+
+	for _, i := range sm {
+		msToExpiration := i.Timestamp - time.Now().UnixMilli()
+
+		premium := i.Price - i.Index
+		yield := premium / i.Index
+		annualisedYield := yield / (float64(msToExpiration) / (1000 * 60 * 60 * 24 * 365))
+
+		if i.Name == "BTC-PERPETUAL" {
+			fmt.Printf("  %-13s %8.2f            %8.6f%%\n", i.Name, premium, i.Funding*100)
+		} else {
+			fmt.Printf("  %-13s %8.2f %6.2f%% %s\n", i.Name, premium, annualisedYield*100, tenor(msToExpiration))
+		}
+	}
 }
 
 func main() {
@@ -44,7 +72,6 @@ func main() {
 		var sm stateMessage
 		json.Unmarshal(message, &sm)
 
-		fmt.Println("\033[2J\033[H\033[?25l") // clear screen, move cursor to top of screen, hide cursor
-		fmt.Println(sm)
+		display(sm)
 	}
 }
